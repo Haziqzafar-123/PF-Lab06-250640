86748801157229010274ccbf66d07a64d8103cb7
Haziqzafar-123 <k250640@nu.edu.pk>
2025-09-29 09:59:48 +0500

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+ int main(){ 
+    int num, fact=1;
+    
+	 printf("Enter a number: "); 
+	 scanf("%d", &num);
+	  while(num>0){ 
+	  fact = fact * num; num--; 
+	  } 
+	  printf("factorial is %d", fact); 
+	  
+}
+
+
